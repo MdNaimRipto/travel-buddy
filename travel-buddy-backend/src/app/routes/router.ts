3d9a313cd29f9d41240f861787f87b0493c32cab
MdNaimRipto <naimurtsc567@gmail.com>
2023-12-01 10:07:07 +0600

    Basic functions added

---
+++ travel-buddy-backend/src/app/routes/router.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const router = express.Router();
+
+// const routes = [
+//   {
+//     path: "/user",
+//     route: UsersRouter,
+//   },
+// ];
+
+// routes.map(r => router.use(r.path, r.route));
+
+export const Routers = router;
