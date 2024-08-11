7260da3c60fb4eb6062485f98088f6cde3705459
MdNaimRipto <naimurtsc567@gmail.com>
2024-08-11 15:13:30 +0600

    Booking Form Created

--- travel-buddy-frontend/src/components/reservations/reservationDetails/bookReservation/BookReservation.tsx
+++ travel-buddy-frontend/src/components/reservations/reservationDetails/bookReservation/BookReservation.tsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import BookCalendar from "./BookCalendar";
 import { colorConfig } from "@/configs/colorConfig";
+import { Button, Divider } from "@mui/material";
+import {
+  calculateTotalNights,
+  formatDateTime,
+  getDateWithTime,
+} from "@/utils/bookReservation/bookReservationUtils";
 
 const BookReservation = () => {
   const today = new Date();
-
-  const getDateWithTime = (date: Date, hours: number, minutes: number) => {
-    const newDate = new Date(date);
-    newDate.setHours(hours, minutes, 0, 0);
-    return newDate;
-  };
-
   const tomorrow = getDateWithTime(new Date(today), 9, 0);
 
   tomorrow.setDate(tomorrow.getDate() + 1);
@@ -27,11 +26,30 @@ const BookReservation = () => {
       color: colorConfig.secondary,
     },
   ]);
+
+  // Calculate the total nights
+  const totalNights = calculateTotalNights(
+    dates[0].startDate,
+    dates[0].endDate
+  );
+
+  // Calculate total price (assuming the price is 2400 per night)
+  const pricePerNight = 2800;
+  const discount = 15;
+  const totalPrice = totalNights * pricePerNight;
+
+  const discountAmount = (totalPrice * discount) / 100;
+  const totalBill = totalPrice - discountAmount;
+
   return (
     <div className="px-3 py-4 border border-lightGray rounded-lg">
-      <h6 className="text-xl text-black font-medium font-inter mt-1 mb-3">
-        Book Reservation
-      </h6>
+      <h3 className="text-lg text-black font-medium mt-2 mb-3 titleFont">
+        BDT{" "}
+        <span className="line-through text-gray text-sm titleFont font-normal">
+          2800
+        </span>{" "}
+        <span className="titleFont">2400 Night</span>
+      </h3>
       <BookCalendar
         today={today}
         tomorrow={tomorrow}
@@ -39,10 +57,71 @@ const BookReservation = () => {
         setDates={setDates}
         getDateWithTime={getDateWithTime}
       />
-      <div>
-        <h5>Check-In: {dates[0].startDate.toLocaleString()}</h5>
-        <h5>Check-Out: {dates[0].endDate.toLocaleString()}</h5>
-      </div>
+      <>
+        <h3 className="text-xl text-black font-medium titleFont mt-5 mb-3">
+          Booking Details
+        </h3>
+        <div className="booking-details p-3">
+          <div className="flex items-center justify-evenly border border-lightGray rounded-lg p-2">
+            <h5 className="flex flex-col gap-2">
+              <span className="text-xs font-medium font-inter text-black">
+                Check-In:
+              </span>{" "}
+              <span className="text-sm font-medium font-poppins text-black">
+                {formatDateTime(dates[0].startDate)}
+              </span>
+            </h5>
+            <span className="h-[50px] w-[1px] bg-lightGray block mx-3"></span>
+            <h5 className="flex flex-col gap-2">
+              <span className="text-xs font-medium font-inter text-black">
+                Check-Out:
+              </span>{" "}
+              <span className="text-sm font-medium font-poppins text-black">
+                {formatDateTime(dates[0].endDate)}
+              </span>
+            </h5>
+          </div>
+          <div className="px-2">
+            <p className="flex items-center justify-between text-sm font-inter font-medium mt-5 mb-2">
+              <span className="underline font-normal">Check-In Time:</span>
+              <span>09:00 AM</span>
+            </p>
+            <p className="flex items-center justify-between text-sm font-inter font-medium mb-2">
+              <span className="underline font-normal">Total Nights:</span>
+              <span>{totalNights}</span>
+            </p>
+            <p className="flex items-center justify-between text-sm font-inter font-medium mb-2">
+              <span className="underline font-normal">Discount:</span>
+              <span>{discount}%</span>
+            </p>
+            <p className="flex items-center justify-between text-sm font-inter font-medium mb-2">
+              <span className="underline font-normal">{`${totalNights} x ${`2800BDT`}:`}</span>
+              <span>{totalPrice}BDT</span>
+            </p>
+            <Divider orientation="horizontal" />
+            <p className="flex items-center justify-between text-sm font-inter font-medium mt-2">
+              <span className="underline font-normal">Total Bill:</span>
+              <span>{totalBill}BDT</span>
+            </p>
+          </div>
+        </div>
+        <Button
+          variant="contained"
+          sx={{
+            width: "100%",
+            py: 1.5,
+            mt: 2,
+            background: `${colorConfig.secondary} !important`,
+            color: colorConfig.white,
+            "&:hover": {
+              background: `${colorConfig.secondary} !important`,
+              color: colorConfig.white,
+            },
+          }}
+        >
+          Book Now
+        </Button>
+      </>
     </div>
   );
 };
