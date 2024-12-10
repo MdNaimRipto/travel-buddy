b6dbdd31d1f69daffb875565142297aa0b35d43b
MdNaimRipto <naimurtsc567@gmail.com>
2024-12-10 22:54:32 +0600

    Facebook & Twitter Provider Page issue fixed

--- travel-buddy-frontend/src/components/auth/authProviderOptions/Facebook.tsx
+++ travel-buddy-frontend/src/components/auth/authProviderOptions/Facebook.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 import CommonProviderBtn from "./CommonProviderBtn";
 import logo from "@/assets/auth/facebook.png";
+import { apiConfig } from "@/configs/apiConfig";
+import { signIn, useSession } from "next-auth/react";
+import { useHandleProviderLogin } from "@/hooks/useProviderLogin";
 
 const Facebook = () => {
-  return <CommonProviderBtn logo={logo} title="Continue With Facebook" />;
+  const checkUserEndPoint = apiConfig.USER.CHECK_PROVIDER_LOGIN;
+
+  const handleFacebookLogin = () => {
+    signIn("facebook", {
+      callbackUrl: "http://localhost:3000/auth/login?method=FACEBOOK",
+    });
+  };
+
+  const { data, status } = useSession();
+
+  useHandleProviderLogin({
+    status,
+    authMethod: "FACEBOOK",
+    checkUserEndPoint,
+    data,
+  });
+
+  return (
+    <CommonProviderBtn
+      logo={logo}
+      title="Continue With Facebook"
+      handleLogin={handleFacebookLogin}
+      authMethod="FACEBOOK"
+    />
+  );
 };
 
 export default Facebook;
--- travel-buddy-frontend/src/components/auth/authProviderOptions/Twitter.tsx
+++ travel-buddy-frontend/src/components/auth/authProviderOptions/Twitter.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 import logo from "@/assets/auth/twitter.png";
 import CommonProviderBtn from "./CommonProviderBtn";
+import { apiConfig } from "@/configs/apiConfig";
+import { signIn, useSession } from "next-auth/react";
+import { useHandleProviderLogin } from "@/hooks/useProviderLogin";
 
 const Twitter = () => {
-  return <CommonProviderBtn logo={logo} title="Continue With Twitter" />;
+  const checkUserEndPoint = apiConfig.USER.CHECK_PROVIDER_LOGIN;
+
+  const handleTwitterLogin = () => {
+    signIn("twitter", {
+      callbackUrl: "http://localhost:3000/auth/login?method=TWITTER",
+    });
+  };
+
+  const { data, status } = useSession();
+
+  useHandleProviderLogin({
+    status,
+    authMethod: "TWITTER",
+    checkUserEndPoint,
+    data,
+  });
+
+  return (
+    <CommonProviderBtn
+      logo={logo}
+      title="Continue With Twitter"
+      handleLogin={handleTwitterLogin}
+      authMethod="TWITTER"
+    />
+  );
 };
 
 export default Twitter;
